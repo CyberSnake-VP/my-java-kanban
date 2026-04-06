57873d1218bc4ba66e0a3242f3e90cf1348e533a
Vadim <zigmundfreid89@yandex.ru>
2026-04-06 23:16:29 +0300

    Реализовать логику Node хранения списка просмотренных задач

--- src/manager/inMemoryHistoryManager.java
+++ src/manager/inMemoryHistoryManager.java
@@ -2,6 +2,7 @@ package manager;
 
 import tasks.Task;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +28,7 @@ public class inMemoryHistoryManager implements HistoryManager{
         }
     }
 
+    // добавление в конец
     private Node addLast(Task task) {
         Node oldTail = tail;
         Node newTail = new Node(oldTail, task, null);
@@ -39,18 +41,71 @@ public class inMemoryHistoryManager implements HistoryManager{
         return newTail;
     }
 
+    // удаление ссылок на Node
+    private void removeNode(Node node) {
+        if(head == tail) {
+            head = null;
+            tail = null;
+            return;
+        }
+        if(node == head) {
+            head = head.next;
+            // Проверка, что есть следующий узел(т.е. голова не единственный узел)
+            if(head != null) {
+                head.prev = null;
+            }
+            return;
+        }
+        if(node == tail) {
+            tail = tail.prev;
+            if(tail != null) {
+                tail.next = null;
+            }
+            return;
+        }
+
+        // перезапись ссылок для других случаев
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
     @Override
     public void add(Task task) {
-
+        if(task == null) {
+            return;
+        }
+        if(history.containsKey(task.getId())) {
+            removeNode(history.get(task.getId()));
+        }
+        history.put(task.getId(), addLast(task));
     }
 
     @Override
     public List<Task> getHistory() {
-        return List.of();
+        // пока голова не равна null добавляем задачу в список
+        // Тем самым я пробегаюсь от головы к хвосту, где сохраняется порядок добавления в историю
+        List<Task> tasks = new ArrayList<>();
+        Node node = head;
+        while(node != null) {
+            tasks.add(node.task);
+            node = node.next;
+        }
+        return tasks;
     }
 
     @Override
     public void remove(int id) {
-
+        // Находим ноду
+        // Удаляем ссылку(связи)
+        // Удаляем из таблицы истории
+        /// если удалить из таблицы ноду это неправильно, потому что в самих Node'ах останутся на нее ссылки,
+        ///  их нужно стереть
+        if(history.containsKey(id)) {
+            Node removed = history.get(id);
+            removeNode(removed);
+            history.remove(id);
+        }
     }
 }
