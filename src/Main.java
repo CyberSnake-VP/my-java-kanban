bc8dc3a88d44d7cd1e67de3a01772c1779afb652
Vadim <zigmundfreid89@yandex.ru>
2026-04-07 17:51:12 +0300

    Закончить тестирование истории просмотров задач

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,19 @@ import tasks.Task;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-       ArrayList<Integer> numbers = new ArrayList<>();
-       numbers.add(1);
-       numbers.add(2);
-       System.out.println(numbers);
-       numbers.set(1, 3);
-       System.out.println(numbers);
-
-       for(Integer number : numbers) {
-           if(number == 1) {
-               numbers.set(1, 23);
-           }
-           System.out.println(number);
-       }
+        TaskManager manager = new InMemoryTaskManager();
+        Task task1 = new Task("1", "description", Instant.now(), Duration.ofMinutes(10));
+        Task task2 = new Task("2", "description", Instant.now(), Duration.ofMinutes(10));
+        task1 = manager.createTask(task1);
+        task2 = manager.createTask(task2);
+        manager.getTask(task1.getId());
+        manager.getTask(task2.getId());
+        manager.getTask(task1.getId());
+        List<Task> tasks = manager.getHistory();
+        System.out.println(tasks);
     }
 }
--- test/manager/InMemoryHistoryManagerTest.java
+++ test/manager/InMemoryHistoryManagerTest.java
@@ -2,8 +2,12 @@ package manager;
 
 import org.junit.jupiter.api.Test;
 import status.Status;
+import tasks.Epic;
+import tasks.Subtask;
 import tasks.Task;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -56,5 +60,108 @@ public class InMemoryHistoryManagerTest extends InMemoryTaskManagerTest{
         int actualValue = tasks.size();
 
         assertEquals(expectedValue, actualValue, "History should have been empty");
+    }
+
+    @Test
+    void addHistoryEpic() {
+        int expectedValue = 1;
+        epic = manager.createEpic(epic);
+        manager.getEpic(epic.getId());
+        List<Task> epics = manager.getHistory();
+        int actualValue = epics.size();
+
+        assertEquals(expectedValue, actualValue, "History should have been empty");
+    }
+
+    @Test
+    void deleteHistoryEpic() {
+        int expectedValue = 0;
+        epic = manager.createEpic(epic);
+        manager.getEpic(epic.getId());
+        manager.deleteEpic(epic.getId());
+        List<Task> epics = manager.getHistory();
+        int actualValue = epics.size();
+
+        assertEquals(expectedValue, actualValue, "History should have been empty");
+    }
+
+    @Test
+    void deleteAllHistoryEpics() {
+        int expectedValue = 0;
+        epic = manager.createEpic(epic);
+        manager.getEpic(epic.getId());
+        manager.deleteAllEpics();
+        List<Task> epics = manager.getHistory();
+        int actualValue = epics.size();
+
+        assertEquals(expectedValue, actualValue, "History should have been empty");
+    }
+
+    @Test
+    void addHistorySubtask() {
+        int expectedValue = 1;
+        epic = manager.createEpic(epic);
+        Subtask subtask = createSubtaskInEpic(epic);
+        manager.getSubtask(subtask.getId());
+        List<Task> subtasks = manager.getHistory();
+        int actualValue = subtasks.size();
+
+        assertEquals(expectedValue, actualValue, "History should have been empty");
+    }
+
+    @Test
+    void deleteHistorySubtask() {
+        int expectedValue = 0;
+        epic = manager.createEpic(epic);
+        Subtask subtask = createSubtaskInEpic(epic);
+        manager.getSubtask(subtask.getId());
+        manager.deleteSubtask(subtask.getId());
+        List<Task> subtasks = manager.getHistory();
+        int actualValue = subtasks.size();
+
+        assertEquals(expectedValue, actualValue, "History should have been empty");
+    }
+
+    @Test
+    void deleteAllHistorySubtasks() {
+        int expectedValue = 0;
+        epic = manager.createEpic(epic);
+        Subtask subtask = createSubtaskInEpic(epic);
+        manager.getSubtask(subtask.getId());
+        manager.deleteAllSubtasks();
+        List<Task> subtasks = manager.getHistory();
+        int actualValue = subtasks.size();
+
+        assertEquals(expectedValue, actualValue, "History should have been empty");
+    }
+
+
+    @Test
+    void shouldBePreservingInsertionOrderInHistory() {
+        String expectedName = "2";
+
+        task = manager.createTask(task);
+        Task task2 = manager.createTask(new Task("2", "description", Instant.now(), Duration.ofMinutes(10)));
+        manager.getTask(task.getId());
+        manager.getTask(task2.getId());
+        List<Task> tasks = manager.getHistory();
+        String actualName = tasks.get(1).getName();
+
+        assertEquals(expectedName, actualName, "History should have been the same");
+    }
+
+    @Test
+    void shouldBeSaveCurrentViewStateInHistory() {
+        String expectedName = "2";
+
+        task = manager.createTask(task);
+        Task task2 = manager.createTask(new Task("2", "description", Instant.now(), Duration.ofMinutes(10)));
+        manager.getTask(task.getId());
+        manager.getTask(task2.getId());
+        manager.getTask(task.getId());
+        List<Task> tasks = manager.getHistory();
+        String actualName = tasks.getFirst().getName();
+
+        assertEquals(expectedName, actualName, "History should have been the same");
     }
 }
