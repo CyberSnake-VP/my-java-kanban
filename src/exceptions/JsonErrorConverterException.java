9e571fe459361fcdc52cd9669be2199998c681bd
Vadim <zigmundfreid89@yandex.ru>
2026-04-13 19:31:52 +0300

    Закончить метод createOrUpdate

---
+++ src/exceptions/JsonErrorConverterException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class JsonErrorConverterException extends RuntimeException {
+    public JsonErrorConverterException(String message) {
+        super(message);
+    }
+}
--- src/httpservers/handlers/BaseHandler.java
+++ src/httpservers/handlers/BaseHandler.java
@@ -4,13 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import exceptions.JsonSyntaxException;
 import httpservers.adapters.DurationAdapter;
 import httpservers.adapters.InstantAdapter;
 import manager.TaskManager;
 
 import java.io.IOException;
-import java.io.PipedReader;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
