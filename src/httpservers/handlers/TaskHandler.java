c117cd849d53e58c5931985d44126ffb53908ec0
Vadim <zigmundfreid89@yandex.ru>
2026-04-14 15:25:10 +0300

    доработан метод createdOrUpdate

--- src/httpservers/handlers/TaskHandler.java
+++ src/httpservers/handlers/TaskHandler.java
@@ -11,6 +11,9 @@ import tasks.Task;
 import java.io.IOException;
 
 public class TaskHandler extends BaseHandler {
+    private static final String NOT_FOUND_MESSAGE = "Такой задачи не существует";
+    private static final String NOT_HAVE_NAME_MESSAGE = "Отсутствует название задачи";
+    private static final String SERIALIZED_EXCEPTION_MESSAGE = "Ошибка при попытке десериализации тела запроса";
 
 
     public TaskHandler(TaskManager manager) {
@@ -26,7 +29,7 @@ public class TaskHandler extends BaseHandler {
     void processPost(String path, HttpExchange exchange) throws IOException {
         try {
             String[] elements = path.split("/");
-            if(elements.length == 2 && elements[1].equals("tasks")) {
+            if (elements.length == 2 && elements[1].equals("tasks")) {
                 createOrUpdate(exchange);
                 return;
             }
@@ -41,52 +44,57 @@ public class TaskHandler extends BaseHandler {
     private void createOrUpdate(HttpExchange exchange) throws IOException, JsonErrorConverterException {
         byte[] body = exchange.getRequestBody().readAllBytes();
         String json = new String(body);
-
         try {
             Task task = jsonMapper.fromJson(json, Task.class);
             // если у задачи отсутствует id будем создавать новую задачу
-            if(task.getId() == null) {
+            if (task.getId() == null) {
                 // валидация на наличие названия задачи
-                if(task.getName() == null) {
-                    throw new JsonErrorConverterException("Отсутствует название задачи");
+                if (task.getName() == null) {
+                    throw new JsonErrorConverterException(NOT_HAVE_NAME_MESSAGE);
                 }
                 // Проверяем статус задачи, если нет, то указываем. Т.к. задачу будем создавать на основе той, что
                 // в json, чтобы статус не стал null.
-                if(task.getStatus() == null) {
+                if (task.getStatus() == null) {
                     task.setStatus(Status.NEW);
                 }
                 // создаем задачу в менеджере
                 Task createdTask = manager.createTask(task);
                 // переводим обратно в json
-                String jsonTask  = jsonMapper.toJson(createdTask, Task.class);
+                String jsonTask = jsonMapper.toJson(createdTask, Task.class);
                 // отправляем на сервер код ответа и json нашей задачи
                 sendResponse(exchange, CREATED, jsonTask);
+                return;
             }
 
             // обновляем задачу
             Task oldTask = manager.getTask(task.getId());
-            if(task.getName() != null) {
+            if (oldTask == null) {
+
+                sendResponse(exchange, NOT_FOUND, NOT_FOUND_MESSAGE);
+                return;
+            }
+            if (task.getName() != null) {
                 oldTask.setName(task.getName());
             }
-            if(task.getDescription() != null) {
+            if (task.getDescription() != null) {
                 oldTask.setDescription(task.getDescription());
             }
-            if(task.getStatus() != null) {
+            if (task.getStatus() != null) {
                 oldTask.setStatus(task.getStatus());
             }
-            if(task.getStartTime() != null) {
+            if (task.getStartTime() != null) {
                 oldTask.setStartTime(task.getStartTime());
             }
-            if(task.getDuration() != null) {
+            if (task.getDuration() != null) {
                 oldTask.setDuration(task.getDuration());
             }
             // может выдать intersectionsException
             task = manager.updateTask(oldTask);
-            String taskJson  = jsonMapper.toJson(task, Task.class);
+            String taskJson = jsonMapper.toJson(task, Task.class);
             sendResponse(exchange, OK, taskJson);
 
         } catch (JsonSyntaxException e) {
-            throw  new JsonErrorConverterException("Ошибка попытке десериализации тела запроса");
+            throw new JsonErrorConverterException(SERIALIZED_EXCEPTION_MESSAGE);
         } catch (IntersectionsException e) {
             sendResponse(exchange, NOT_ACCEPTABLE, e.getMessage());
         }
