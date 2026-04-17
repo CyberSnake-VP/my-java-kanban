55a86fd08cfcba90f49427da8e77799e020f6091
Vadim <zigmundfreid89@yandex.ru>
2026-04-17 18:34:27 +0300

    Закончить тесты Task, Epic, Subtask

--- test/httpserver/HttpEpicHandlerTest.java
+++ test/httpserver/HttpEpicHandlerTest.java
@@ -1,18 +1,20 @@
 package httpserver;
 
+import com.google.gson.reflect.TypeToken;
 import manager.Managers;
 import manager.TaskManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import status.Status;
 import tasks.Epic;
 
 import java.io.IOException;
-import java.net.http.HttpRequest;
+import java.lang.reflect.Type;
 import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class HttpEpicHandlerTest extends HttpBaseHandlerTest<TaskManager> {
 
@@ -36,6 +38,92 @@ public class HttpEpicHandlerTest extends HttpBaseHandlerTest<TaskManager> {
         List<Epic> epics = manager.getEpics();
         assertEquals(1, epics.size(), "Должен быть один эпик");
         assertEquals("epic", epics.getFirst().getName(), "название не соответствуют");
+    }
+
+    @Test
+    void getEpicTest() throws IOException, InterruptedException {
+        String expectedName = "epic";
+        String expectedDescription = "description";
+        Status expectedStatus = Status.NEW;
+
+        String json = jsonMapper.toJson(epic);
+        sendPostRequest(URI_EPIC, json);
+        HttpResponse<String> res = sendGetRequest(URI_EPIC_BY_ID);
+        Epic actualEpic = jsonMapper.fromJson(res.body(), Epic.class);
+        String actualName = actualEpic.getName();
+        String actualDescription = actualEpic.getDescription();
+        Status actualStatus = actualEpic.getStatus();
+
+        assertEquals(expectedName, actualName, "Имена отличаются");
+        assertEquals(expectedDescription, actualDescription, "Описание отличается");
+        assertEquals(expectedStatus, actualStatus, "Статус задач отличается");
+    }
+
+    @Test
+    void updateEpicTest() throws IOException, InterruptedException {
+        String expectedName = "update";
+        String expectedDescription = "update";
+
+        String json = jsonMapper.toJson(epic);
+        HttpResponse<String> resp = sendPostRequest(URI_EPIC, json);
+        assertEquals(201, resp.statusCode(), "Код ответа не совпадает");
+        HttpResponse<String> res = sendGetRequest(URI_EPIC_BY_ID);
+        Epic actualEpic = jsonMapper.fromJson(res.body(), Epic.class);
+        actualEpic.setName(expectedName);
+        actualEpic.setDescription(expectedDescription);
+        String updateJson = jsonMapper.toJson(actualEpic);
+        HttpResponse<String> res2 = sendPostRequest(URI_EPIC, updateJson);
+        assertEquals(200,  res2.statusCode(), "Кот ответа отличается");
+
+        actualEpic =  jsonMapper.fromJson(res2.body(), Epic.class);
+        String actualName = actualEpic.getName();
+        String actualDescription = actualEpic.getDescription();
+
+        assertEquals(expectedName, actualName, "Название отличается");
+        assertEquals(expectedDescription, actualDescription, "Описание отличается");
+
+        Epic epic = manager.getEpic(actualEpic.getId());
+        assertEquals(expectedName, epic.getName());
+        assertEquals(expectedDescription, epic.getDescription());
+
+    }
+
+    @Test
+    void deleteEpicTest() throws IOException, InterruptedException {
+        String json = jsonMapper.toJson(epic);
+        HttpResponse<String> resp = sendPostRequest(URI_EPIC, json);
+        assertEquals(201, resp.statusCode(), "Статус не совпадает");
+        HttpResponse<String> delResp = sendDeleteRequest(URI_EPIC_BY_ID);
+        assertEquals(204, delResp.statusCode(), "Статус не совпадает");
+
+        Epic epic = jsonMapper.fromJson(resp.body(), Epic.class);
+        int id =  epic.getId();
+        assertNull(manager.getEpic(id), "Эпик должен быть удален");
+    }
+
+    @Test
+    void getAllEpicsTest() throws IOException, InterruptedException {
+        String expectedName = "epic";
+        String expectedDescription = "description";
+        Status expectedStatus = Status.NEW;
+
+        String json = jsonMapper.toJson(epic);
+        sendPostRequest(URI_EPIC, json);
+        HttpResponse<String> resp = sendGetRequest(URI_EPIC);
+        Type epicsList = new TypeToken<List<Epic>>(){}.getType();
+        List<Epic> actualEpics = jsonMapper.fromJson(resp.body(), epicsList);
+        String actualName = actualEpics.getFirst().getName();
+        String actualDescription = actualEpics.getFirst().getDescription();
+        Status actualStatus = actualEpics.getFirst().getStatus();
+        List<Epic> epics = manager.getEpics();
+        String actualNameInManager =  epics.getFirst().getName();
+        String actualDescriptionInManager =  epics.getFirst().getDescription();
+
+        assertEquals(expectedName, actualName, "Название отличаются");
+        assertEquals(expectedDescription, actualDescription, "Описание отличается");
+        assertEquals(expectedStatus, actualStatus, "Статус отличается");
+        assertEquals(expectedName, actualNameInManager, "Названия отличаются");
+        assertEquals(expectedDescription, actualDescriptionInManager, "Описание отличается");
     }
 
 
--- test/httpserver/HttpTaskHandlerTest.java
+++ test/httpserver/HttpTaskHandlerTest.java
@@ -1,12 +1,16 @@
 package httpserver;
 
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonToken;
 import manager.Managers;
 import manager.TaskManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import status.Status;
 import tasks.Task;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.Instant;
@@ -41,6 +45,91 @@ public class HttpTaskHandlerTest extends HttpBaseHandlerTest<TaskManager> {
 
         assertEquals(1, taskFromManager.size(), "Количество задач не совпадает");
         assertEquals("task", taskFromManager.getFirst().getName());
+    }
+
+    @Test
+    void updateTaskTest() throws IOException, InterruptedException {
+        String expectedName = "update";
+        String expectedDescription = "update";
+        Status exceptedStatus = Status.IN_PROGRESS;
+
+        String json = jsonMapper.toJson(task);
+        HttpResponse<String> respPost = sendPostRequest(URI_TASK, json);
+        assertEquals(201, respPost.statusCode(), "код ответа не верный");
+
+        Task taskForUpdate = jsonMapper.fromJson(respPost.body(), Task.class);
+        taskForUpdate.setName(expectedName);
+        taskForUpdate.setDescription(expectedDescription);
+        taskForUpdate.setStatus(exceptedStatus);
+        String updatedJson = jsonMapper.toJson(taskForUpdate);
+        HttpResponse<String> resp = sendPostRequest(URI_TASK, updatedJson);
+        assertEquals(200, resp.statusCode(), "Код ответа не совпадает");
+
+        List<Task> tasks = manager.getTasks();
+        Task updatedTask = tasks.getFirst();
+        String actualName = updatedTask.getName();
+        String actualDescription = updatedTask.getDescription();
+        Status actualStatus = updatedTask.getStatus();
+
+        assertEquals(expectedName, actualName, "Название задачи не совпадает");
+        assertEquals(expectedDescription, actualDescription, "Описание задачи не совпадает");
+        assertEquals(exceptedStatus, actualStatus, "Статус не совпадает");
+    }
+
+    @Test
+    void deleteTaskTest() throws IOException, InterruptedException {
+        int expectedCount = 0;
+        String json = jsonMapper.toJson(task);
+        sendPostRequest(URI_TASK, json);
+        HttpResponse<String> response = sendDeleteRequest(URI_TASK_BY_ID);
+        assertEquals(204, response.statusCode(), "Код ответа не совпадает");
+        List<Task> tasks = manager.getTasks();
+        int actualCount = tasks.size();
+        assertEquals(expectedCount, actualCount, "Задача не была удалена");
+    }
+
+    @Test
+    void getAllTasksTest() throws IOException, InterruptedException {
+        String expectedName = "task";
+        String expectedDescription = "description";
+        Status expectedStatus = Status.NEW;
+
+        String json = jsonMapper.toJson(task);
+        sendPostRequest(URI_TASK, json);
+        HttpResponse<String> response = sendGetRequest(URI_TASK);
+        assertEquals(200, response.statusCode(), "Код ответа не совпадает");
+        List<Task> tasks = manager.getTasks();
+        // для работы со списками и дженериками, нужно использовать указания типа, для правильно десериализации, через TypeToken<List<Task>>
+        Type taskListType = new TypeToken<List<Task>>() {
+        }.getType();
+        List<Task> actualTasks = jsonMapper.fromJson(response.body(), taskListType);
+        assertEquals(tasks.size(), actualTasks.size(), "количество задач не совпадает");
+        String actualName = actualTasks.getFirst().getName();
+        String actualDescription = actualTasks.getFirst().getDescription();
+        Status actualStatus = actualTasks.getFirst().getStatus();
+
+        assertEquals(expectedName, actualName, "название задачи не совпадает");
+        assertEquals(expectedDescription, actualDescription, "описание задачи отличается");
+        assertEquals(expectedStatus, actualStatus, "Статус задачи отличается");
+    }
+
+    @Test
+    void getTaskByIdTest() throws IOException, InterruptedException {
+        String expectedName = "task";
+        String expectedDescription = "description";
+        Status expectedStatus = Status.NEW;
+
+        String json = jsonMapper.toJson(task);
+        sendPostRequest(URI_TASK, json);
+        HttpResponse<String> response = sendGetRequest(URI_TASK_BY_ID);
+        Task getTask =  jsonMapper.fromJson(response.body(), Task.class);
+        String actualName = getTask.getName();
+        String actualDescription = getTask.getDescription();
+        Status actualStatus = getTask.getStatus();
+
+        assertEquals(expectedName, actualName, "название не совпадает");
+        assertEquals(expectedDescription, actualDescription, "описание не совпадает");
+        assertEquals(expectedStatus, actualStatus, "статус не совпадает");
     }
 
 }
